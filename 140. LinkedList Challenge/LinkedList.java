0fc4267d30a4f192158dbf75684bf4d5271308d2
injeolmeeeeeee <86404260+injeolmeeeeeee@users.noreply.github.com>
2023-11-11 09:15:02 +0800

    commit

---
+++ 140. LinkedList Challenge/LinkedList.java
@@ -0,0 +1,17 @@
+public class LinkedList {
+    public static void main(String[] args) {
+        LinkedList<String> placesToVisit = new LinkedList<>();
+
+        placesToVisit.add("Sydney");
+        placesToVisit.add("Melbourne");
+        placesToVisit.add("Brisbane");
+        placesToVisit.add("Adelaide");
+        placesToVisit.add("Alice Springs");
+        placesToVisit.add("Perth");
+        placesToVisit.add("Darwin");
+
+        System.out.println(placesToVisit);
+
+    }
+    
+}
